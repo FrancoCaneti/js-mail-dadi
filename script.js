79dd20b1350056336feb2b25c39a2aa68b309803
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-22 16:22:31 +0100

    daDI

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 var emailUtente = prompt ('Inserisci la tua mail').toLocaleLowerCase();
 console.log ('La tua mail è: ',emailUtente);
 
-// Lista Mail
-var listaEmail = ['tonystark@starkindustries.us', 'cap1942@avenger.it', 'hulk@smash.boom', 'god_of_the:thunder2@thor.asg']
+// Lista Mail 
+var listaEmail = ['tonystark@starkindustries.us', 'cap1942@avenger.us', 'hulk@smash.boom', 'god_of_the:thunder2@thor.asg']
 console.log ('Database Email: ',listaEmail);
 
 //Controllo Mail
@@ -25,9 +25,36 @@ if (mailFound === true) {
     console.log('Autenticazione fallita! Avviare protocollo di sicurezza!')
 }
 
+document.getElementById('mailFound').innerHTML = (mailFound)
+
+
+//*************************************************************************************//
 
 
 //Esercizio Dadi
 
-var
- 
+
+//Numero Random Utente
+var numeroUtente = Math.floor(Math.random()*6)+1;
+console.log ('Numero  dado giocatore: ',numeroUtente);
+
+//Numero Random Computer
+var numeroComputer = Math.floor(Math.random()*6)+1;
+console.log ('Numero  dado computer:',numeroComputer);
+
+//Vincitore
+var risultato ;
+
+
+if (numeroUtente > numeroComputer){
+   console.log (risultato = 'hai vinto')
+} else if (numeroUtente < numeroComputer){
+   console.log (risultato = 'Hai perso') 
+} else if (numeroUtente == numeroComputer){
+   console.log (risultato = 'Pareggio')
+}
+
+
+
+
+document.getElementById('risultato').innerHTML = ('Yazi: '+ risultato)
